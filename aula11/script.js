e9edb5a3da5b0a3c68d6b909f0332d4ea761dae9
Lucas Henrique de Oliveira Vieira <lucashenriquevieira86@gmail.com>
2025-10-24 23:32:52 +0000

    add aula11 e aula12

---
+++ aula11/script.js
@@ -0,0 +1,5 @@
+console.log("Texto 1")
+
+const h1 = document.querySelector("#titulo")
+
+h1.innerText = "Texto modificado com JavaScript"
---
+++ aula12/script.js
@@ -0,0 +1,11 @@
+const nome = "Lucas Henrique"
+let idade = 19
+idade = 20
+
+const produto = {
+    nome:"Lucas Henrique",
+    valor: 18
+}
+
+produto.valor = 22
+console.log(produto)
